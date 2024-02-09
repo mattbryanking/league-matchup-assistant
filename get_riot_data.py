c8b72d655704b393626018b7d483e6abb8959955
Matthew King <matthewkingstudent@gmail.com>
2024-02-09 17:01:13 -0500

    implemented gpt api - need to look into gpt 4

--- get_riot_data.py
+++ get_riot_data.py
@@ -4,103 +4,122 @@ import json
 from dotenv import load_dotenv
 from roleidentification import pull_data, get_roles
 
-region_to_routing = {
-    'na1': 'AMERICAS',
-    'br1': 'AMERICAS',
-    'la1': 'AMERICAS',
-    'la2': 'AMERICAS',
-    'oc1': 'SEA',
-    'eun1': 'EUROPE',
-    'euw1': 'EUROPE',
-    'jp1': 'ASIA',
-    'kr': 'ASIA',
-    'ru': 'EUROPE',
-    'tr1': 'EUROPE'
-}
 
-# read champion data from json file
-f = open('champion.json', 'r', encoding='utf-8')
-champion_data = json.load(f)
+def get_matchup(game_name, tag_line, region):
 
-load_dotenv()
+    region_to_routing = {
+        'na1': 'AMERICAS',
+        'br1': 'AMERICAS',
+        'la1': 'AMERICAS',
+        'la2': 'AMERICAS',
+        'oc1': 'SEA',
+        'eun1': 'EUROPE',
+        'euw1': 'EUROPE',
+        'jp1': 'ASIA',
+        'kr': 'ASIA',
+        'ru': 'EUROPE',
+        'tr1': 'EUROPE'
+    }
 
-# get request variables
-api_key = os.getenv('RIOT_DEV_KEY')
-game_name = "Jiminnn"
-tag_line = "NA1"
-region = 'na1'
-routing = region_to_routing[region]
-riot_id = f"{game_name}/{tag_line}"
+    # read champion data from json file
+    f = open('champion.json', 'r', encoding='utf-8')
+    champion_data = json.load(f)
 
-# set up headers, include token in headers
-headers = {
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": api_key
-}
+    load_dotenv()
 
-# search for puuid via riot id
-print(f"Searching for user via Riot ID...")
-response = requests.get(
-    f'https://{region_to_routing[region]}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{riot_id}', headers=headers)
+    # get request variables
+    api_key = os.getenv('RIOT_DEV_KEY')
+    routing = region_to_routing[region]
+    riot_id = f"{game_name}/{tag_line}"
 
-player_puuid = response.json()['puuid']
-if not player_puuid:
-    print(f"Could not find user with Riot ID {game_name}#{tag_line}")
-    exit()
+    # set up headers, include token in headers
+    headers = {
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": api_key
+    }
 
-print(f"User found with Riot ID: {game_name}#{tag_line}")
-print(f"PUUID: {player_puuid}")
-print(f"Requesting encrypted Summoner ID...")
+    # search for puuid via riot id
+    print(f"Searching for user via Riot ID...")
+    response = requests.get(
+        f'https://{region_to_routing[region]}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{riot_id}', headers=headers)
 
-response = requests.get(
-    f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{player_puuid}', headers=headers)
-summoner_id = response.json()['id']
+    player_puuid = response.json()['puuid']
+    if not player_puuid:
+        print(f"Could not find user with Riot ID {game_name}#{tag_line}")
+        exit()
 
-if not summoner_id:
-    print("Could not find summoner ID.")
-    exit()
+    print(f"User found with Riot ID: {game_name}#{tag_line}")
+    print(f"PUUID: {player_puuid}")
+    print(f"Requesting encrypted Summoner ID...")
 
-print(f"Encrypted Summoner ID: {summoner_id}")
-print(f"Gathering current match data...")
+    response = requests.get(
+        f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{player_puuid}', headers=headers)
+    summoner_id = response.json()['id']
 
-# request spectator data from riot api
-response = requests.get(
-    f'https://{region}.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}', headers=headers)
-match_data = response.json()
+    if not summoner_id:
+        print("Could not find summoner ID.")
+        exit()
 
-with open('current_match.json', 'w') as f:
-    json.dump(match_data, f)
+    print(f"Encrypted Summoner ID: {summoner_id}")
+    print(f"Gathering current match data...")
 
-# find current player's data
-if not match_data:
-    print("Player is not currently in a match.")
-    exit()
+    # request spectator data from riot api
+    response = requests.get(
+        f'https://{region}.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}', headers=headers)
+    match_data = response.json()
 
-# create dictionary as {puuid: [team, champion_id, champion_name, role]}
-player_champ_data = {}
+    if response.status_code != 200 or not match_data:
+        print("User is not currently in a valid match.")
+        exit()
+    
+    with open('current_match.json', 'w') as f:
+        json.dump(match_data, f)
 
-# for each player in the match, add their champion id and team to the dictionary
-for player in match_data['participants']:
-    player_champ_data[player['puuid']] = [
-        player['teamId'], player['championId']]
+    print("Match data found.")
+    print("Calculating player matchup...")
+    
+    # create dictionary as {puuid: [team, champion_id, role]}
+    player_champ_data = {}
 
-# for each champion id in dictionary, find the champion name
-for puuid in player_champ_data:
-    champion_id = str(player_champ_data[puuid][0])
-    for champion in champion_data['data']:
-        if champion_data['data'][champion]['key'] == champion_id:
-            player_champ_data[puuid].append(champion)
+    # for each player in the match, add their champion id and team to the dictionary
+    for player in match_data['participants']:
+        player_champ_data[player['puuid']] = [
+            player['teamId'], player['championId']]
+
+    # create arrays of champion ids for each team for role identification
+    team1 = []
+    team2 = []
+
+    # add champion ids to team arrays
+    for puuid in player_champ_data:
+        if player_champ_data[puuid][0] == 100:
+            team1.append(player_champ_data[puuid][1])
+        else:
+            team2.append(player_champ_data[puuid][1])
+
+    # get roles for each player
+    champion_roles = pull_data()
+    team1_roles = get_roles(champion_roles, team1)
+    team2_roles = get_roles(champion_roles, team2)
+
+    # add roles to player_champ_data dictionary
+    for puuid, (team, champion_id) in player_champ_data.items():
+        roles_dict = team1_roles if team == 100 else team2_roles
+        role = next((role for role, champ_id in roles_dict.items()
+                    if champ_id == champion_id), None)
+        if role:
+            player_champ_data[puuid].append(role)
+            
+    # find players role and determine matchup
+    player_data = player_champ_data[player_puuid]
+    enemy_data = None
+    
+    # loop through players to find enemy player in same role
+    for puuid, data in player_champ_data.items():
+        if data[0] != player_data[0] and data[2] == player_data[2]:
+            enemy_data = data
             break
+    
 
-# create list of enemy team's champion ids
-champion_roles = pull_data()
-enemy_champs = []
-
-for puuid in player_champ_data:
-    if player_champ_data[puuid][0] != player_champ_data[player_puuid][0]:
-        enemy_champs.append(player_champ_data[puuid][1])
-
-
-print(f"Enemy team's champions: {enemy_champs}")
---
+++ main.py
@@ -0,0 +1,9 @@
+from get_riot_data import get_matchup
+from get_gpt_response import get_gpt_response
+
+def main():
+    # get_matchup("Jiminnn", "NA1", "na1")
+    # get_gpt_response()
+    
+if __name__ == "__main__":
+    main()
